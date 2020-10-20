efcb3c8a0a737fa896101395a3295fb7af664aba
Anthony Norman <anthnorm@gmail.com>
2020-10-20 15:00:04 +0200

    Lab 11 klar

--- JS/Lekt-7/Lab 11/script.js
+++ JS/Lekt-7/Lab 11/script.js
@@ -5,6 +5,21 @@
 // själv söka information om hur du använder detta.
 // Tänk på att klockan ska uppdateras 1 gång i sekunden så att det alltid visar rätt!
 //
-const myDate = new Date();
 
-console.log(myDate)
+const myInterval = setInterval(() => { 
+    timer()
+ }, 1000);
+  
+ function dater(){
+    const myDate = new Date();
+    const date =myDate.setUTCDate();
+    document.getElementById("datum").innerHTML = myDate;
+    // console.log(time)
+ }
+ function timer() {
+    const myTime = new Date();
+    const time = myTime.toLocaleTimeString();
+    document.getElementById("clock").innerHTML = myTime;
+    // console.log(time)
+};
+ 
