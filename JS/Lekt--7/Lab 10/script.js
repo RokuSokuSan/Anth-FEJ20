464f2e7a9204623ae67015d8bc66725457c3117b
Anthony Norman <anthnorm@gmail.com>
2020-10-20 13:59:57 +0200

    Change name Lab 7

--- JS/Lekt--7/Lab 10/script.js
+++ JS/Lekt--7/Lab 10/script.js
@@ -16,14 +16,3 @@ const circle = (diameter=0) => diameter / 2;
     let areaOf= circle(500)*circle(500)*Math.PI;
     console.log('The area of the diameter is:', areaOf)
 //End laboration 10
-
-
-// Start laboration 11
-// I den här uppgiften ska du skapa en hemsida som visar dagens datum och den aktuella tiden.
-// Du ska skapa ett grafiskt gränssnitt med html och css men hur det ska se ut väljer du själv. För att
-// hämta tid och datum bör du använda dig av Date objektet som finns inbyggt i Javascript men du får
-// själv söka information om hur du använder detta.
-// Tänk på att klockan ska uppdateras 1 gång i sekunden så att det alltid visar rätt!
-//
-const myDate = new Date();
-console.log(myDate)
---
+++ JS/Lekt--7/Lab 11/script.js
@@ -0,0 +1,10 @@
+// Start laboration 11
+// I den här uppgiften ska du skapa en hemsida som visar dagens datum och den aktuella tiden.
+// Du ska skapa ett grafiskt gränssnitt med html och css men hur det ska se ut väljer du själv. För att
+// hämta tid och datum bör du använda dig av Date objektet som finns inbyggt i Javascript men du får
+// själv söka information om hur du använder detta.
+// Tänk på att klockan ska uppdateras 1 gång i sekunden så att det alltid visar rätt!
+//
+const myDate = new Date();
+
+console.log(myDate)
