6173a36ec0f981fc082cf4eb10b95ed843a97d92
Anthony Norman <anthnorm@gmail.com>
2020-10-23 13:53:19 +0200

    Extra-1

---
+++ JS/Lek-8/Extra-Pupil/js/script.js
@@ -0,0 +1,18 @@
+// Extrauppgift
+// I den här uppgiften vill jag att ni ska hjälpa alla stackars lärare där ute!
+// Jag vill att ni bygger en sida där man kan lägga till elever i en lista och markera i om dom är
+// närvarande eller inte.
+// När man har skrivit i elevens namn i en input och trycker på enter eller submit så ska detta namn
+// automatiskt dyka upp i fönstret så man enkelt kan läsa ut vilka elever som är tilllagda.
+// Där vill jag även att man enkelt ska kunna ändra på varje elev om dom är närvarande eller inte, om
+// det skulle bli fel.
+// När man är färdig med dagens närvarolista så vill jag även att ni kikar på hur man kan spara dagens
+// lista för att enkelt kunna kolla tillbaka på föregående dagar och eventuellt skicka in till respektive
+// myndighet.
+// En bonus är om ni lyckas spara listan med elever så att den finns kvar varje gång man går in på sidan
+// utan att man behöver skriva in alla igen. Tänk dock på att det bör gå att ta bort elever som kanske
+// inte längre går kvar på utbildningen.
+
+class Pupil (){
+    cons
+}
